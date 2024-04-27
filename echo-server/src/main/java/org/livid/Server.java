0a74abbbbc0a55b24ba3e1ae8870edcfd40c53a8
haril song <songkg7@gmail.com>
2024-04-27 15:20:46 +0900

    thread pool

--- echo-server/src/main/java/org/livid/Server.java
+++ echo-server/src/main/java/org/livid/Server.java
@@ -1,11 +1,10 @@
 package org.livid;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.logging.Logger;
 
 public class Server {
@@ -14,14 +13,17 @@ public class Server {
 
     public static void main(String[] args) {
         // server
-        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+        try (
+                ServerSocket serverSocket = new ServerSocket(PORT);
+                ExecutorService executor = Executors.newFixedThreadPool(10)
+        ) {
             LOGGER.info("Server is running on port " + PORT);
 
             // Accept multiple clients sequentially
             //noinspection InfiniteLoopStatement
             while (true) {
                 Socket clientSocket = serverSocket.accept();
-                new Thread(new ClientHandler(clientSocket)).start();
+                executor.submit(new ClientHandler(clientSocket));
             }
         } catch (IOException e) {
             LOGGER.severe("Could not listen on port " + PORT + ": " + e.getMessage());
